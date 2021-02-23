dab9f8a4abd5b0c414d0f1295da45b4d85d62d43
eduardo <appiopini@gmail.com>
2021-02-23 16:44:21 -0300

    resolved animation side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-import jQuery from 'jquery'
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +9,20 @@ import jQuery from 'jquery'
 })
 export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
-  ngAfterViewInit( ){
+
+  ngAfterViewInit(){
     (function($) {
-        "use strict";
-    
-        // Add active state to sidbar nav links
+      "use strict";
         var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-                if (this.href === path) {
-                    $(this).addClass("active");
-                }
-            });
-    
-        // Toggle the side navigation
-        $("#sidebarToggle").on("click", function(e) {
-            e.preventDefault();
-            $("body").toggleClass("sb-sidenav-toggled");
-        });
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (this.getAttribute("href") === path) {
+                  $(this).addClass("active");
+              }
+          });   
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
     })(jQuery);
   }
 
